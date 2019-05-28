8d9c835365d3f8ca44221ac15915511bdec1195d
amin <amin137718@gmail.com>
2019-05-28 23:33:16 +0430

    startIT

---
+++ testS/type ha.py
@@ -0,0 +1,16 @@
+num = 1000   #int
+new_num = 10.5   #float
+new_list = ["hayola", 'yohaha', 'hehehe']  # list ast
+new_set = {11,  23, 423}  #set ast
+
+
+
+#new_list [0] = 100
+#print (new_list [0])
+#print (type (num))
+#print (type (new_num))
+#print (type (new_list))
+#print (type(new_set))
+#print (len(new_list))
+#print (len(new_set))
+#print (new_list[0])
