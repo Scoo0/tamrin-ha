5d922c2afda50cf4c661473398a35b2c6f04265f
amin <amin137718@gmail.com>
2019-07-06 15:26:18 +0430

    doreee

---
+++ tamrine kelase 2/7 ..py
@@ -0,0 +1,9 @@
+txt = "hello"
+word = "whatever"
+
+text2 = txt.capitalize() # harfe avali bozorg
+word = word.capitalize()
+txt3 = txt.upper() #hame harfa bozorg
+txt4 = txt.lower() #hame harfa kochak
+.count() # dar tuple nshan midade tedad tekrar
+.index() #neshan dadne adadie makan dar tuple
